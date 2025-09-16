9f99d6283d739f7a92ce9a3319b3a8d5673d6e65
drawiks <147206917+drawiks@users.noreply.github.com>
2025-09-16 22:22:54 +0300

    .

---
+++ src/routes/home.py
@@ -0,0 +1,44 @@
+
+import flet as ft
+
+class HomeRoute:
+    def __init__(self):
+        #post
+        self.post_column = ft.Column(spacing=20)
+        self.post_field = ft.TextField(label="Что у вас нового?", multiline=True, max_lines=10, width=750)
+    
+    def build(self, navigation_rail):
+        return ft.View(
+            "/",
+            [
+                ft.Container(
+                    content=ft.Row(
+                        [
+                            navigation_rail,
+                            ft.Column(
+                                [
+                                    ft.Text(
+                                        "Опубликовать пост",
+                                        size=50,
+                                        color=ft.Colors.WHITE,
+                                        weight=ft.FontWeight.W_100,
+                                        text_align=ft.TextAlign.CENTER,
+                                        font_family="plex medium"
+                                    ),
+                                    self.post_field,
+                                    ft.Row([
+                                            ft.ElevatedButton("Опубликовать"),
+                                            ft.IconButton(icon=ft.Icons.ADD_PHOTO_ALTERNATE_OUTLINED),
+                                            ft.IconButton(icon=ft.Icons.ATTACH_FILE)
+                                    ]),
+                                    ft.Divider(thickness=1),
+                                    self.post_column,
+                                ]
+                            )
+                        ],
+                    ),
+                    expand=True
+                ),
+                
+            ]
+        )
